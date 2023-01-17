eaf79eeedc5921f944a6dd5b8a0c1178251b4514
Juan Ayme <juanayme565@gmail.com>
2023-01-17 15:43:31 -0500

    creacion de paquetes

---
+++ src/main/java/com/tiendaapip/marketplat/MarketPlatApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaapip.marketplat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaapip/marketplat/MarketPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaapip.marketplat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
